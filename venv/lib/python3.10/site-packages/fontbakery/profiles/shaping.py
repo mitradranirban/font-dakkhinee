4e7562e2f2b13d65bc2a6e41ce5f2e978d9ee49c
Dr Anirban Mitra <mitradranirban@gmail.com>
2024-07-18 19:49:47 +0530

    New font proof

---
+++ venv/lib/python3.10/site-packages/fontbakery/profiles/shaping.py
@@ -0,0 +1,11 @@
+PROFILE = {
+    "sections": {
+        "Shaping Checks": [
+            "com.google.fonts/check/shaping/regression",
+            "com.google.fonts/check/shaping/forbidden",
+            "com.google.fonts/check/shaping/collides",
+            "com.google.fonts/check/dotted_circle",
+            "com.google.fonts/check/soft_dotted",
+        ]
+    }
+}
---
+++ venv/lib/python3.10/site-packages/fontbakery/profiles/ufo.py
@@ -0,0 +1,16 @@
+PROFILE = {
+    "name": "UFO Sources",
+    "sections": {
+        "UFO Sources": [
+            "com.daltonmaag/check/ufolint",
+            "com.daltonmaag/check/ufo_required_fields",
+            "com.daltonmaag/check/ufo_recommended_fields",
+            "com.daltonmaag/check/ufo_unnecessary_fields",
+            "com.google.fonts/check/designspace_has_sources",
+            "com.google.fonts/check/designspace_has_default_master",
+            "com.google.fonts/check/designspace_has_consistent_glyphset",
+            "com.google.fonts/check/designspace_has_consistent_codepoints",
+            "com.thetypefounders/check/features_default_languagesystem",
+        ]
+    },
+}
